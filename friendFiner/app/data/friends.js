cb9fb573318401c72c8bed3eeaf49b15341fd05e
Iz <pehisaac@gmail.com>
2018-04-20 10:06:19 -0600

    First go through, updating project to a whole..notha..level!

--- friendFiner/app/data/friends.js
+++ friendFiner/app/data/friends.js
@@ -18,3 +18,8 @@
 //     ]
 // }
 // ```
+
+var friends = [];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
--- friendFiner/app/routing/apiRoutes.js
+++ friendFiner/app/routing/apiRoutes.js
@@ -1,8 +1,38 @@
-// // 4. Your `apiRoutes.js` file should contain two routes:
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to our "data" sources.
+// These data sources hold arrays of information on friends and their choices
+// ===============================================================================
 
-// //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-// //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+var friendData = require("../data/friends");
 
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/friends... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // Then the server saves the data to the friendData array)
+  // ---------------------------------------------------------------------------
+
+
+  // //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+
+  app.post("/api/tables", function(req, res) {
+    friendData.push(req.body);
 
 
 // 6. Determine the user's most compatible friend using the following as a guide:
@@ -15,3 +45,17 @@
 //        * Total Difference: **2 + 1 + 2 =** **_5_**
 //    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on. 
 //    * The closest match will be the user with the least amount of difference.
+
+
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    friendData = [];
+
+    console.log(friendData);
+  });
+};
